3105a328ab75ba9a30fb6d7d4aed2a5f9eb05d9e
AnwarCaravantes <165080319+AnwarCaravantes@users.noreply.github.com>
2025-08-11 14:32:36 -0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'uNIDataSet.Empleado' table. You can move, or remove it, as needed.
+            this.empleadoTableAdapter.Fill(this.uNIDataSet.Empleado);
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                this.Validate();
+                this.empleadoBindingSource.EndEdit();
+
+
+                this.empleadoTableAdapter.Update(this.uNIDataSet.Empleado);
+
+                MessageBox.Show("Información guardada exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al guardar la información: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void label7_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
